79637b86f32f593ec00d00e325ae03c1beee8b4c
ReubenSebastian <23083000055@student.unmer.ac.id>
2025-10-04 11:12:15 +0700

    fix: supabase client + dynamic layout 3

---
+++ app/login/layout.jsx
@@ -0,0 +1,7 @@
+// app/login/layout.jsx
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export default function LoginLayout({ children }) {
+  return children;
+}
---
+++ app/signup/layout.jsx
@@ -0,0 +1,7 @@
+// app/signup/layout.jsx
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export default function SignupLayout({ children }) {
+  return children;
+}
